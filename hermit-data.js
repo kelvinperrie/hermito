853cae18d060ab1aa906f17765682945b8a4e4c4
kelvin <kelvinperrie@gmail.com>
2026-01-24 21:22:30 +1300

    Initial commit

---
+++ hermit-data.js
@@ -0,0 +1,46 @@
+var crabData = [
+    {
+        commonName : "",
+        scientificName : "example",
+        aka : [],
+        memoirNiwaPage : 123123123,
+        natureWatchLink: "https://inaturalist.nz/taxa/something",
+        attributes : [
+            { key: "example" , values : ["square","round"] }
+        ],
+        details : [
+            { text: "Example text.", references: [1]}
+        ],
+        similarTo: [
+            { key: "Example", label: "Example Common Name" }
+        ],
+        images : [
+            { url : "images/crabs/something.jpg", "attribution" : { "name" : "Some due", "link" : "https://inaturalist.nz/observations/zzzzzzz", "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/" } }
+        ],
+        references : [
+            { id: 1, detail : "EXAMPLE Morton, J., Miller, M. (1973) The New Zealand Sea Shore (pp89). Collins, London 1973" }
+        ]
+    },
+        {
+        commonName : "",
+        scientificName : "Areopaguristes setosus",
+        aka : [],
+        memoirNiwaPage : 73,
+        natureWatchLink: "https://inaturalist.nz/taxa/636902-Areopaguristes-setosus",
+        attributes : [
+            { key: "example" , values : ["square","round"] }
+        ],
+        details : [
+            { text: "Example text.", references: [1]}
+        ],
+        similarTo: [
+            { key: "Example", label: "Example Common Name" }
+        ],
+        images : [
+            { url : "images/crabs/Areopaguristes setosus 01.jpg", "attribution" : { "name" : "lcolmer", "link" : "https://inaturalist.nz/observations/62745403", "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/" } }
+        ],
+        references : [
+            { id: 1, detail : "EXAMPLE Morton, J., Miller, M. (1973) The New Zealand Sea Shore (pp89). Collins, London 1973" }
+        ]
+    }
+]
