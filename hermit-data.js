45198f9cbff3c23ee00f1a75831d9413a3fae604
kelvin <kelvinperrie@gmail.com>
2026-01-24 22:10:05 +1300

    adding data + images

--- hermit-data.js
+++ hermit-data.js
@@ -42,5 +42,181 @@ var crabData = [
         references : [
             { id: 1, detail : "EXAMPLE Morton, J., Miller, M. (1973) The New Zealand Sea Shore (pp89). Collins, London 1973" }
         ]
+    },
+        {
+        commonName : "",
+        scientificName : "Diacanthurus spinulimanus",
+        aka : [],
+        memoirNiwaPage : 135,
+        natureWatchLink: "https://inaturalist.nz/taxa/497510-Diacanthurus-spinulimanus",
+        attributes : [
+            { key: "example" , values : ["square","round"] }
+        ],
+        details : [
+            { text: "Example text.", references: [1]}
+        ],
+        similarTo: [
+            { key: "Example", label: "Example Common Name" }
+        ],
+        images : [
+            { url : "images/crabs/Diacanthurus spinulimanus 01.jpg", "attribution" : { "name" : "Emilie & Victoria", "link" : "https://inaturalist.nz/observations/149610051", "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/" } }
+        ],
+        references : [
+            { id: 1, detail : "EXAMPLE Morton, J., Miller, M. (1973) The New Zealand Sea Shore (pp89). Collins, London 1973" }
+        ]
+    },
+        {
+        commonName : "",
+        scientificName : "Diacanthurus rubricatus",
+        aka : [],
+        memoirNiwaPage : 139,
+        natureWatchLink: "https://inaturalist.nz/taxa/514925-Diacanthurus-rubricatus",
+        attributes : [
+            { key: "example" , values : ["square","round"] }
+        ],
+        details : [
+            { text: "Example text.", references: [1]}
+        ],
+        similarTo: [
+            { key: "Example", label: "Example Common Name" }
+        ],
+        images : [
+            { url : "images/crabs/Diacanthurus rubricatus 01.jpg", "attribution" : { "name" : "Shaun", "link" : "https://inaturalist.nz/observations/149610051", "licenseUrl" : "https://creativecommons.org/licenses/by/4.0/" } }
+        ],
+        references : [
+            { id: 1, detail : "EXAMPLE Morton, J., Miller, M. (1973) The New Zealand Sea Shore (pp89). Collins, London 1973" }
+        ]
+    },
+        {
+        commonName : "",
+        scientificName : "Dardanus arrosor",
+        aka : [],
+        memoirNiwaPage : 81,
+        natureWatchLink: "https://inaturalist.nz/taxa/466901-Dardanus-arrosor",
+        attributes : [
+            { key: "example" , values : ["square","round"] }
+        ],
+        details : [
+            { text: "Example text.", references: [1]}
+        ],
+        similarTo: [
+            { key: "Example", label: "Example Common Name" }
+        ],
+        images : [
+            { url : "images/crabs/Dardanus arrosor 01.jpg", "attribution" : { "name" : "nakileigh", "link" : "https://inaturalist.nz/observations/156184588", "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/" } }
+        ],
+        references : [
+            { id: 1, detail : "EXAMPLE Morton, J., Miller, M. (1973) The New Zealand Sea Shore (pp89). Collins, London 1973" }
+        ]
+    },
+        {
+        commonName : "",
+        scientificName : "Dardanus longior",
+        aka : [],
+        memoirNiwaPage : null,
+        natureWatchLink: "https://inaturalist.nz/taxa/1286751-Dardanus-longior",
+        attributes : [
+            { key: "example" , values : ["square","round"] }
+        ],
+        details : [
+            { text: "Example text.", references: [1]}
+        ],
+        similarTo: [
+            { key: "Example", label: "Example Common Name" }
+        ],
+        images : [
+            { url : "images/crabs/Dardanus longior 01.jpg", "attribution" : { "name" : "pcaiger", "link" : "https://inaturalist.nz/observations/297119369", "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/" } }
+        ],
+        references : [
+            { id: 1, detail : "EXAMPLE Morton, J., Miller, M. (1973) The New Zealand Sea Shore (pp89). Collins, London 1973" }
+        ]
+    },
+        {
+        commonName : "",
+        scientificName : "Dardanus sanguinolentus",
+        aka : [],
+        memoirNiwaPage : null,
+        natureWatchLink: "https://inaturalist.nz/taxa/1287081-Dardanus-sanguinolentus",
+        attributes : [
+            { key: "example" , values : ["square","round"] }
+        ],
+        details : [
+            { text: "Example text.", references: [1]}
+        ],
+        similarTo: [
+            { key: "Example", label: "Example Common Name" }
+        ],
+        images : [
+            { url : "images/crabs/Dardanus sanguinolentus 01.jpg", "attribution" : { "name" : "Malcolm Francis", "link" : "https://inaturalist.nz/observations/252476447", "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/" } }
+        ],
+        references : [
+            { id: 1, detail : "EXAMPLE Morton, J., Miller, M. (1973) The New Zealand Sea Shore (pp89). Collins, London 1973" }
+        ]
+    },
+        {
+        commonName : "",
+        scientificName : "Paguristes barbatus",
+        aka : [],
+        memoirNiwaPage : 59,
+        natureWatchLink: "https://inaturalist.nz/taxa/472058-Paguristes-barbatus",
+        attributes : [
+            { key: "example" , values : ["square","round"] }
+        ],
+        details : [
+            { text: "Example text.", references: [1]}
+        ],
+        similarTo: [
+            { key: "Example", label: "Example Common Name" }
+        ],
+        images : [
+            { url : "images/crabs/Paguristes barbatus 01.jpg", "attribution" : { "name" : "tangatawhenua", "link" : "https://inaturalist.nz/observations/4235848", "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/" } }
+        ],
+        references : [
+            { id: 1, detail : "EXAMPLE Morton, J., Miller, M. (1973) The New Zealand Sea Shore (pp89). Collins, London 1973" }
+        ]
+    },
+        {
+        commonName : "Queensland Hermit Crab",
+        scientificName : "Calcinus sirius",
+        aka : [],
+        memoirNiwaPage : null,
+        natureWatchLink: "https://inaturalist.nz/taxa/1080999-Calcinus-sirius",
+        attributes : [
+            { key: "example" , values : ["square","round"] }
+        ],
+        details : [
+            { text: "Example text.", references: [1]}
+        ],
+        similarTo: [
+            { key: "Example", label: "Example Common Name" }
+        ],
+        images : [
+            { url : "images/crabs/Calcinus sirius 01.jpg", "attribution" : { "name" : "pcaiger", "link" : "https://inaturalist.nz/observations/297120139", "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/" } }
+        ],
+        references : [
+            { id: 1, detail : "EXAMPLE Morton, J., Miller, M. (1973) The New Zealand Sea Shore (pp89). Collins, London 1973" }
+        ]
+    },
+        {
+        commonName : "",
+        scientificName : "Lophopagurus thompsoni",
+        aka : [],
+        memoirNiwaPage : 157,
+        natureWatchLink: "https://inaturalist.nz/taxa/603299-Lophopagurus-thompsoni",
+        attributes : [
+            { key: "example" , values : ["square","round"] }
+        ],
+        details : [
+            { text: "Example text.", references: [1]}
+        ],
+        similarTo: [
+            { key: "Example", label: "Example Common Name" }
+        ],
+        images : [
+            { url : "images/crabs/Lophopagurus thompsoni 01.jpg", "attribution" : { "name" : "Luca Davenport-Thomas", "link" : "https://inaturalist.nz/observations/243039360", "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/" } }
+        ],
+        references : [
+            { id: 1, detail : "EXAMPLE Morton, J., Miller, M. (1973) The New Zealand Sea Shore (pp89). Collins, London 1973" }
+        ]
     }
 ]
